1d82f6040b13352b10922f4bb33c36826ef69caa
NihilisticPandemonium <whiro@tukui.org>
2020-01-07 06:55:57 -0800

    Clean this up.

--- ElvUI/Modules/ActionBars/ActionBars.lua
+++ ElvUI/Modules/ActionBars/ActionBars.lua
@@ -863,8 +863,8 @@ function AB:DisableBlizzard()
 	self:SecureHook('BlizzardOptionsPanel_OnEvent')
 	--InterfaceOptionsFrameCategoriesButton6:SetScale(0.00001)
 
-	for _, frame in pairs({"MainMenuBar", "StanceBarFrame", "PossessBarFrame", "PETACTIONBAR_YPOS", "MULTICASTACTIONBAR_YPOS", 	"MultiBarBottomLeft", "MultiBarBottomRight", "MultiCastActionBarFrame", "ExtraActionBarFrame"}) do
-		if _G[frame] and type(_G[frame]) == "table" then
+	for _, frame in pairs({"MainMenuBar", "StanceBarFrame", "PossessBarFrame", "MultiBarBottomLeft", "MultiBarBottomRight", "MultiCastActionBarFrame", "ExtraActionBarFrame"}) do
+		if _G[frame] then
 			_G[frame]:ClearAllPoints();
 			_G[frame].SetPoint = E.noop;
 			_G[frame].ClearAllPoints = E.noop;
